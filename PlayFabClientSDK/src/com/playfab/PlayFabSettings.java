0e14a8c126badbd4704828ca347e98aeae6d6ec5
pgilmorepf <paul@playfab.com>
2015-11-20 16:33:21 -0800

    Client settings no longer contains DeveloperSecretKey

--- PlayFabClientSDK/src/com/playfab/PlayFabSettings.java
+++ PlayFabClientSDK/src/com/playfab/PlayFabSettings.java
@@ -7,7 +7,6 @@ public class PlayFabSettings {
     public static String TitleId = null;
     public static ErrorCallback GlobalErrorHandler;
     public static String LogicServerURL = null;
-    public static String DeveloperSecretKey = null;
 
 
     public static String GetLogicURL() {
