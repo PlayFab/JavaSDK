979b137c211bc5017ce92f576d65fc50407373b2
playfabjenkinsbot <playfabjenkinsbot@github.com>
2016-08-18 22:53:18 +0000

    Automated pf-main build from Jenkins

--- PlayFabServerSDK/src/com/playfab/PlayFabSettings.java
+++ PlayFabServerSDK/src/com/playfab/PlayFabSettings.java
@@ -4,7 +4,7 @@ import com.playfab.PlayFabErrors.ErrorCallback;
 
 public class PlayFabSettings {
     public static String SdkVersion = "0.33.160815";
-    public static String BuildIdentifier = "jbuild_javasdk_0";
+    public static String BuildIdentifier = "jbuild_javasdk_1";
     public static String SdkVersionString = "JavaSDK-0.33.160815";
 
     public static String TitleId = null; // You must set this value for PlayFabSdk to work properly (Found in the Game Manager for your title, at the PlayFab Website)
