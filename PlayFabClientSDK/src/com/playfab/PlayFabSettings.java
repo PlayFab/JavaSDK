bf138f6fbd38e230be78ad95845f5a993411921d
playfabjenkinsbot <playfabjenkinsbot@github.com>
2016-08-22 17:09:56 +0000

    Automated build from Jenkins

--- PlayFabClientSDK/src/com/playfab/PlayFabSettings.java
+++ PlayFabClientSDK/src/com/playfab/PlayFabSettings.java
@@ -3,9 +3,9 @@ package com.playfab;
 import com.playfab.PlayFabErrors.ErrorCallback;
 
 public class PlayFabSettings {
-    public static String SdkVersion = "0.33.160815";
+    public static String SdkVersion = "0.34.160822";
     public static String BuildIdentifier = "jbuild_javasdk_0";
-    public static String SdkVersionString = "JavaSDK-0.33.160815";
+    public static String SdkVersionString = "JavaSDK-0.34.160822";
 
     public static String TitleId = null; // You must set this value for PlayFabSdk to work properly (Found in the Game Manager for your title, at the PlayFab Website)
     public static ErrorCallback GlobalErrorHandler;
