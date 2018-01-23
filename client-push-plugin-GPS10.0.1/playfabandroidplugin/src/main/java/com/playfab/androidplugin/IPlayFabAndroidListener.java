0313f61c34bb882cf91400571bd71ab2af02fc99
Paul Gilmore <paul@playfab.com>
2018-01-23 15:48:08 -0800

    Delete unsupported push plugin
    Summary: Delete unsupported push plugin
    
    Test Plan: Technically none, but Jenker! will make sure there's no issues with the deletion
    
    Reviewers: PGilmore
    
    Maniphest Tasks: T3589
    
    Differential Revision: https://phabricator.playfab.com/D5272

--- client-push-plugin-GPS10.0.1/playfabandroidplugin/src/main/java/com/playfab/androidplugin/IPlayFabAndroidListener.java
+++
@@ -1,9 +0,0 @@
-package com.playfab.androidplugin;
-
-import com.playfab.androidplugin.GCM.PlayFabNotificationPackage;
-
-public interface IPlayFabAndroidListener {
-    void onGCMReady();
-    void onGCMTokenRefreshed(String token);
-    void onGCMMessageRecieved(PlayFabNotificationPackage token);
-}
