506d45eaae90975f4516053c77a9a287d85caf44
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2022-07-04 20:18:55 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#220704

--- PlayFabServerSDK/src/main/java/com/playfab/PlayFabSettings.java
+++ PlayFabServerSDK/src/main/java/com/playfab/PlayFabSettings.java
@@ -9,7 +9,7 @@ import com.playfab.PlayFabErrors.ErrorCallback;
 
 public class PlayFabSettings {
     public static String SdkVersion = "0.162.220704";
-    public static String BuildIdentifier = "adobuild_javasdk_118";
+    public static String BuildIdentifier = "adobuild_javasdk_116";
     public static String SdkVersionString = "JavaSDK-0.162.220704";
 
     public static Map<String, String> RequestGetParams;
