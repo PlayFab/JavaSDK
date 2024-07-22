ce1281ee560acb6611c5c1eaeecdcf9adce8cf2d
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2024-07-22 21:39:48 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#240719

--- PlayFabServerSDK/packageMe.ps1
+++ PlayFabServerSDK/packageMe.ps1
@@ -5,4 +5,4 @@ New-Item -ItemType Directory -Force ./builds
 popd
 
 cd target
-Copy-Item  server-sdk-0.219.240621.jar -Destination ../../builds/server-sdk-0.219.240621.jar
+Copy-Item  server-sdk-0.220.240719.jar -Destination ../../builds/server-sdk-0.220.240719.jar
