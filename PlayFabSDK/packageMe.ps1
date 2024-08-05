485143b82afb24d13bb76b579395edf606ddfbea
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2024-08-05 22:19:54 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#240802

--- PlayFabSDK/packageMe.ps1
+++ PlayFabSDK/packageMe.ps1
@@ -5,4 +5,4 @@ New-Item -ItemType Directory -Force ./builds
 popd
 
 cd target
-Copy-Item  combo-sdk-0.220.240719.jar -Destination ../../builds/combo-sdk-0.220.240719.jar
+Copy-Item  combo-sdk-0.221.240802.jar -Destination ../../builds/combo-sdk-0.221.240802.jar
--- PlayFabSDK/packageMe.sh
+++ PlayFabSDK/packageMe.sh
@@ -7,4 +7,4 @@ mkdir -p ./builds
 popd
 
 cd target
-cp  combo-sdk-0.220.240719.jar ../../builds/combo-sdk-0.220.240719.jar
+cp  combo-sdk-0.221.240802.jar ../../builds/combo-sdk-0.221.240802.jar
